f178b4172d517fed835f73be26744c250b32993e
okonri <ikeclemens@gmail.com>
2021-10-31 09:06:25 -0400

    travis file extended

---
+++ build/precache-manifest.ca8b42e624d5116f6deb995f426d7cc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcf6d2f2828c9e6cfaff",
+    "url": "/classier-canada/COMP3104/static/css/main.2f7d5e83.chunk.css"
+  },
+  {
+    "revision": "bcf6d2f2828c9e6cfaff",
+    "url": "/classier-canada/COMP3104/static/js/main.bcf6d2f2.chunk.js"
+  },
+  {
+    "revision": "4b237d9267c1d4db98f4",
+    "url": "/classier-canada/COMP3104/static/js/runtime~main.4b237d92.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/classier-canada/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "2b208a16d32d48f3471f0903b76cb268",
+    "url": "/classier-canada/COMP3104/index.html"
+  }
+];
